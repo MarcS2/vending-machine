2fcd3cd1765662338e6d745f2eb56808e41df92d
MarcS2 <marcellosharrai@hotmail.com>
2023-10-03 16:36:46 -0600

    vendr

--- app/AppState.js
+++ app/AppState.js
@@ -16,7 +16,7 @@ class ObservableAppState extends EventEmitter {
     new Snack({ name: 'Spaghetti', price: 8.75, imgUrl: 'https://images.unsplash.com/photo-1635264685671-739e75e73e0f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80' }),
     new Snack({ name: 'Raw Catfish', price: 15.25, imgUrl: 'https://images.unsplash.com/photo-1515735543535-12664d2453f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80' }),
     new Snack({ name: 'Whole Pineapple', price: 6.75, imgUrl: 'https://images.unsplash.com/photo-1550828520-4cb496926fc9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1933&q=80' }),
-    new Snack({ name: 'Wheat Grain', price: 25.50, imgUrl: 'https://images.unsplash.com/photo-1555064837-3c7ae70f81be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2hlYXQlMjBncmFpbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' })
+    new Snack({ name: 'Wheat Grain', price: 25.45, imgUrl: 'https://images.unsplash.com/photo-1555064837-3c7ae70f81be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2hlYXQlMjBncmFpbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' })
   ]
   money = 20
   // !SECTION
